44590875c44d9e04cccc563de0e8719911398ab2
Nisarg Rawal <nisarg_rawal@brown.edu>
2016-06-09 21:02:32 -0400

    Finished part 2 of the tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello World! You are at the polls index!")
+
+def fuck(request):
+	return HttpResponse("What the fuck bro??!!")
